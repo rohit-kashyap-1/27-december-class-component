15e541de2798b2c352944f64d291e0c8c1d43358
rohit-kashyap-1 <r23.tgc@gmail.com>
2024-01-03 11:21:23 +0530

    03 Jan 2023 - useEffect() with Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import UserDetails from './components/JAN03/UserDetails';
 function App() {
   const [users, setUsers] = useState([]) //users.length = 0
   const [user, setUser] = useState(null)
@@ -38,24 +39,9 @@ function App() {
         {console.log(userId)}
 
       </div>
+      {(user != null) ?<UserDetails user={user} /> : ''}
 
-      {(user != null) ?
-        <div className='w-50 border p-4 rounded'>
-          <h4 className='mb-0'>User Details</h4>
-          <img src={user.image} width={100} />
-          <p className='mb-0'><strong>Full Name:</strong> {user.firstName + ' ' + user.lastName}</p>
-          <p className='mb-0'><strong>Email:</strong> {user.email}</p>
-          <p className='mb-0'><strong>Tel:</strong> {user.phone}</p>
-          <p className='mb-0'><strong>Gender:</strong> {user.gender}</p>
-          <p className='mb-0'><strong>Age:</strong> {user.age + ' Years old'}</p>
-          <p className='mb-0'><strong>Username:</strong> {user.username}</p>
-          <p className='mb-0'><strong>Password:</strong> {user.password}</p>
-          <p className='mb-0'><strong>Address:</strong> {user.address.address + ', ' + user.address.city}</p>
-
-
-
-        </div>
-        : ''}
+      
     </div>
   );
 }
--- src/components/JAN03/UserDetails.js
+++ src/components/JAN03/UserDetails.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
-export default function UserDetails() {
+export default function UserDetails({user}) {
   return (
-    <div>UserDetails</div>
+    <div>
+        {(user != null) ?
+        <div className='w-50 border p-4 rounded'>
+          <h4 className='mb-0'>User Details</h4>
+          <img src={user.image} width={100} />
+          <p className='mb-0'><strong>Full Name:</strong> {user.firstName + ' ' + user.lastName}</p>
+          <p className='mb-0'><strong>Email:</strong> {user.email}</p>
+          <p className='mb-0'><strong>Tel:</strong> {user.phone}</p>
+          <p className='mb-0'><strong>Gender:</strong> {user.gender}</p>
+          <p className='mb-0'><strong>Age:</strong> {user.age + ' Years old'}</p>
+          <p className='mb-0'><strong>Username:</strong> {user.username}</p>
+          <p className='mb-0'><strong>Password:</strong> {user.password}</p>
+          <p className='mb-0'><strong>Address:</strong> {user.address.address + ', ' + user.address.city}</p>
+
+
+
+        </div>
+        : ''}
+    </div>
   )
 }
